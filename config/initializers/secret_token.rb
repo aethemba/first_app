7e36e799ba9a3ce5ab4b711745b87bfae955c3b7
Aksel Ethem <aksel.ethem@gmail.com>
2014-06-27 14:05:23 +0200

    added first commit of ruby scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '83f07d54bf001ecec5e85b6520c780d7b68a4cecd993d4852080c13dc81580cc4b955eb403c5d1d2d1aee6e66065b2196c0fc849c4e15cb9d6cb881ae506ea61'
